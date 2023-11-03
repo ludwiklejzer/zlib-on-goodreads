4d50649ac8fa3fb72473a254addbb9a4a4d165b2
Ludwik Lejzer <ludwiklejzercc@gmail.com>
2023-11-03 15:07:12 -0300

    First version

---
+++ zlib-on-goodreads.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Get book on Z-Library
+// @namespace    https://raw.githubusercontent.com/ludwiklejzer/zlib-on-goodreads
+// @version      0.1
+// @description  Change "Buy on Amazon" button for "Download on Z-Library"
+// @author       ludwiklejzer (https://github.com/ludwiklejzer)
+// @match        https://*.goodreads.com/book/*
+// @supportURL   https://raw.githubusercontent.com/ludwiklejzer/zlib-on-goodreads/issues
+// @homepageURL  https://github.com/ludwiklejzer/zlib-on-goodreads
+// @updateURL    https://raw.githubusercontent.com/ludwiklejzer/zlib-on-goodreads/main/tabbusca.user.js
+// @downloadURL  https://raw.githubusercontent.com/ludwiklejzer/zlib-on-goodreads/main/tabbusca.user.js
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  let originalBtn;
+  const newBtn = document.createElement("div");
+  const span = document.createElement("span");
+  const bookName = document.querySelector( "[data-testid='bookTitle']",).innerText;
+  const url = `https://z-library.se/s/?q=${encodeURI(bookName)}`;
+
+  span.innerText = "Download on Z-Library";
+  span.classList.add("Button__labelItem");
+  newBtn.classList.add(
+    "Button",
+    "Button--buy",
+    "Button--medium",
+    "Button--block",
+  );
+  newBtn.onclick = () => window.open(url, "_blank").focus();
+  newBtn.appendChild(span);
+
+  originalBtn = document.querySelector( "button.Button.Button--buy.Button--medium.Button--block",);
+  originalBtn.replaceWith(newBtn);
+})();
